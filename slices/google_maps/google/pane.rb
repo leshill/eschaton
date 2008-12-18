66569d38251c0dd80ecedb009c367bc1bef0d675
Guilio Karadanais <karadanais@gmail.com>
2008-12-18 21:36:11 +0200

    Jou ma se Google::Pane! Duidelik!

--- slices/google_maps/google/pane.rb
+++ slices/google_maps/google/pane.rb
@@ -1,8 +1,9 @@
 module Google
-  
+
+  # Represents a pane that can be added to the map using Map#add_pane. Useful when building your own map controls that 
+  # contain html.
   class Pane < MapObject
     
-    #
     # ==== Options:
     #
     # * +text+ - Optional
@@ -28,6 +29,17 @@ module Google
         google_options = pane_options.to_google_options(:dont_convert => [:position])
         self << "#{self.var} = new GooglePane(#{google_options});"      
       end
+    end
+    
+    # Replaces the html of the pane. Works in the same way as JavascriptGenerator#replace_html
+    #
+    #  pane.replace_html "This is new html"
+    #  pane.replace_html :text => "This is new html"    
+    #  pane.replace_html :partial => 'new_html'
+    def replace_html(options)
+      text = options.extract(:text) if options.is_a?(Hash)
+
+      self.script.replace_html self.var, text || options
     end
 
   end
--- test/pane_test.rb
+++ test/pane_test.rb
@@ -8,29 +8,29 @@ class PaneTest < Test::Unit::TestCase
     Eschaton.with_global_script do |script|
       map = Google::Map.new
       
-      assert_output_fixture 'pane = new GooglePane({cssClass: "pane", id: "pane", position: new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(10, 10)), text: "Poly Jean Harvey"});
+      assert_output_fixture 'pane = new GooglePane({cssClass: "pane", id: "pane", position: new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(10, 10)), text: "Poly Jean Harvey is indeed a unique women"});
                              map.addControl(pane);',
                             script.record_for_test {
-                              map.add_control Google::Pane.new(:text => 'Poly Jean Harvey')
+                              map.add_control Google::Pane.new(:text => 'Poly Jean Harvey is indeed a unique women')
                             }
                             
-      assert_output_fixture 'pane = new GooglePane({cssClass: "pane", id: "pane", position: new GControlPosition(G_ANCHOR_TOP_RIGHT, new GSize(10, 10)), text: "Poly Jean Harvey"});
+      assert_output_fixture 'pane = new GooglePane({cssClass: "pane", id: "pane", position: new GControlPosition(G_ANCHOR_TOP_RIGHT, new GSize(10, 10)), text: "Poly Jean Harvey is indeed a unique women"});
                              map.addControl(pane);',                            
                             script.record_for_test {
-                              map.add_control Google::Pane.new(:text => 'Poly Jean Harvey', :anchor => :top_right)
+                              map.add_control Google::Pane.new(:text => 'Poly Jean Harvey is indeed a unique women', :anchor => :top_right)
                             }
                             
-      assert_output_fixture 'pane = new GooglePane({cssClass: "pane", id: "pane", position: new GControlPosition(G_ANCHOR_TOP_RIGHT, new GSize(10, 30)), text: "Poly Jean Harvey"});
+      assert_output_fixture 'pane = new GooglePane({cssClass: "pane", id: "pane", position: new GControlPosition(G_ANCHOR_TOP_RIGHT, new GSize(10, 30)), text: "Poly Jean Harvey is indeed a unique women"});
                              map.addControl(pane);',                            
                             script.record_for_test {                            
-                              map.add_control Google::Pane.new(:text => 'Poly Jean Harvey', :anchor => :top_right,
+                              map.add_control Google::Pane.new(:text => 'Poly Jean Harvey is indeed a unique women', :anchor => :top_right,
                                                                :offset => [10, 30])
                             }
                             
-      assert_output_fixture 'pane = new GooglePane({cssClass: "green", id: "pane", position: new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(10, 10)), text: "Poly Jean Harvey"});
+      assert_output_fixture 'pane = new GooglePane({cssClass: "green", id: "pane", position: new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(10, 10)), text: "Poly Jean Harvey is indeed a unique women"});
                              map.addControl(pane);',                            
                             script.record_for_test {
-                              map.add_control Google::Pane.new(:text => 'Poly Jean Harvey', :css_class => :green)
+                              map.add_control Google::Pane.new(:text => 'Poly Jean Harvey is indeed a unique women', :css_class => :green)
                             }
                             
       assert_output_fixture 'pane = new GooglePane({cssClass: "green", id: "pane", position: new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(10, 10)), text: "test output for render"});
@@ -43,18 +43,39 @@ class PaneTest < Test::Unit::TestCase
   
   def test_pane_id
     Eschaton.with_global_script do |script|
-      output = 'my_pane = new GooglePane({cssClass: "pane", id: "my_pane", position: new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(10, 10)), text: "Poly Jean Harvey"});'
+      output = 'my_pane = new GooglePane({cssClass: "pane", id: "my_pane", position: new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(10, 10)), text: "Poly Jean Harvey is indeed a unique women"});'
      
       assert_output_fixture output,
                             script.record_for_test {
-                              Google::Pane.new(:var => :my_pane, :text => 'Poly Jean Harvey')
+                              Google::Pane.new(:var => :my_pane, :text => 'Poly Jean Harvey is indeed a unique women')
                             }
 
       assert_output_fixture output, 
                             script.record_for_test {
-                              Google::Pane.new(:var => 'my_pane', :text => 'Poly Jean Harvey')
+                              Google::Pane.new(:var => 'my_pane', :text => 'Poly Jean Harvey is indeed a unique women')
                             }
     end
+  end
+  
+  def test_replace_html
+    Eschaton.with_global_script do
+      pane = Google::Pane.new(:text => 'Poly Jean Harvey is indeed a unique women')
+
+      assert_output_fixture '$("pane").update("This is new html");',
+                            Eschaton.with_global_script  {
+                              pane.replace_html :text => "This is new html" 
+                            }
+
+      assert_output_fixture '$("pane").update("This is new html");',
+                            Eschaton.with_global_script {
+                              pane.replace_html "This is new html" 
+                            }
+
+      assert_output_fixture '$("pane").update("test output for render");',
+                            Eschaton.with_global_script {
+                              pane.replace_html :partial => 'new_html' 
+                            }                        
+    end    
   end
   
 end
