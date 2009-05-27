da90bad11844aec8ffe4612480384c2f36be6b73
Guilio Karadanais <karadanais@gmail.com>
2009-05-27 23:00:44 +0200

    Avoid Rails >= 2.3 constant errors

--- lib/eschaton.rb
+++ lib/eschaton.rb
@@ -2,7 +2,7 @@ require "#{File.dirname(__FILE__)}/eschaton/eschaton"
 require "#{File.dirname(__FILE__)}/eschaton/script_store"
 
 Dir["#{File.dirname(__FILE__)}/eschaton/**/*.rb"].each do |file|
-  Eschaton.dependencies.require_or_load file
+  Eschaton.dependencies.require file
 end
 
 Eschaton::SliceLoader.load
